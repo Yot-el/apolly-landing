70c38cb0c79c35282f5d0acfa4bfb08af107a66c
Yot-el <yvsuteots@gmail.com>
2023-08-28 08:08:07 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import { RouterView } from 'vue-router'
+	import MainHeader from '@/components/MainHeader.vue'
+	import MainFooter from '@/components/MainFooter.vue'
+</script>
+
+<template>
+	<MainHeader class="app__header" />
+	<RouterView class="app__main" />
+	<MainFooter class="app__footer" />
+</template>
+
+<style lang="scss" scoped>
+	.app__header {
+		position: absolute;
+		top: 20px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+	<main class="home">
+		<h1 class="visually-hidden">Home page</h1>
+	</main>
+</template>
+
+<style lang="scss" scoped>
+	.home {
+		flex-grow: 1;
+	}
+
+	.home__hero {
+		height: 100dvh;
+		padding: 120px 0 80px 0;
+	}
+</style>
